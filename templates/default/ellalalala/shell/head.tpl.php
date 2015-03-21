75cebf5c1c01e1b70e7ed82dbefeff6848fd634a
elladurham <Ella@Ellas-iMac.local>
2015-03-20 20:55:22 -0500

    Added Foundation and Normalize Stylesheets to Theme
    Linked to foundation.min.css and normalize.css in the shell head to
    begin theming ellalalala in Foundation

--- templates/default/ellalalala/shell/head.tpl.php
+++ templates/default/ellalalala/shell/head.tpl.php
@@ -1,1 +1,3 @@
+<link href="<?= \Idno\Core\site()->config()->getDisplayURL() ?>Themes/Ellalalala/css/foundation.min.css" rel="stylesheet">
+<link href="<?= \Idno\Core\site()->config()->getDisplayURL() ?>Themes/Ellalalala/css/normalize.css" rel="stylesheet">
 <link href="<?= \Idno\Core\site()->config()->getDisplayURL() ?>Themes/Ellalalala/css/default.css" rel="stylesheet">
