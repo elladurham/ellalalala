aefc98d561f407dc3cbb227a642bbbcf30f4fbbe
elladurham <Ella@Ellas-iMac.local>
2015-03-20 21:01:12 -0500

    Images and Mockup Homepage in Foundation Framework
    Just a quick, static prototype of the homepage with some images of my
    work and projects.

--- templates/default/ellalalala/home.tpl.php
+++ templates/default/ellalalala/home.tpl.php
@@ -1,39 +1,65 @@
 <div class="row">
-    <div class="span6 offset2">
-        <h2>Featured Project</h2>
-        <div class="span4">
-            <img src="http://placehold.it/600x600">
-        </div>
-        <?php
-            echo "this is where a project goes";
-        ?>
+      <div class="large-12 columns">
+        <h1>hello<span>iam</span>ella</h1>
+        <p>Very brief intro, <a>consectetur</a> adipisicing elit. Aut <a>reprehenderit</a> quos sequi nulla distinctio. <a>Commodi</a> minus nisi. Someone likes pie. It is me.</p>
+      </div>
     </div>
-    <div class="span4">
-        <h2>Working On</h2>
-        <?php
-            echo "this is where a working on feed goes";
-        ?>
+
+    <div class="row">
+      <div class="large-5 columns">
+        <img src="/Themes/Ellalalala/img/comb-honey-featured.jpg">
+      </div>
+      <div class="large-3 columns">
+        <p>
+          <img src="/Themes/Ellalalala/img/comb-honey-package-and-booklet.jpg">
+        </p>
+        <img src="/Themes/Ellalalala/img/comb-honey-booklet.jpg">
+
+      </div>
+      <div class="large-4 columns">
+        <h2><a>Comb Honey Package &amp; Info Booklet</a></h2>
+        <p>On a bit of a whim I decided to sell some of my extra stores of comb honey. I didn&rsquo;t really think about how this meant I&rsquo;d be handing my endeared honey over to complete strangers. I knew I needed to whip up some quick packaging to showcase the comb in the way it deserved. The booklet was just for kicks.</p>
+      </div>
     </div>
-</div>
-<div class="row">
-    <div class="span4 offset2">
-        <h2>Writings</h2>
-        <?php
-            echo "this is where writings will go";
-        ?>
+    
+    <div class="row">
+      <div class="large-12 columns">
+        <h2>Sketches</h2>
+      </div>
     </div>
-    <div class="span6">
-        <h2>Recent Photos</h2>
+    <div class="row">
+      <div class="large-4 column">
+       <img src="/Themes/Ellalalala/img/rein-hand-lettering.jpg">
+      </div>
+      <div class="large-4 column">
+       <img src="/Themes/Ellalalala/img/empower-voices-nebraskans-for-civic-reform.jpg">
+      </div>
+      <div class="large-4 column">
+       <img src="/Themes/Ellalalala/img/heart-oozing-kindness.jpg">
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="large-6 columns">
+        <h2>Internet Ramblings</h2>
+        <ul>
+          <li><a>Dear Person Who Set Out a Cooler of Ice Cold Water for Those on the Billy Wolf Trail on September 20, 2014:</a></li>
+          <li><a>Lessons in Gardening</a></li>
+          <li><a>Progress not Perfection</a></li>
+        </ul>
+      </div>
+      <div class="large-6 columns">
+        <h2>Postworthy Photos</h2>
         <div class="row">
-            <div class="span2">
-                <img src="http://placehold.it/300x300">
-            </div>
-            <div class="span2">
-                <img src="http://placehold.it/300x300">
-            </div>
-            <div class="span2">
-                <img src="http://placehold.it/300x300">
-            </div>
+          <div class="large-4 columns">
+            <img src="/Themes/Ellalalala/img/good-life-halfsy.jpg">
+          </div>
+          <div class="large-4 columns">
+            <img src="/Themes/Ellalalala/img/busy-bees.jpg">
+          </div>
+          <div class="large-4 columns">
+            <img src="/Themes/Ellalalala/img/dot-and-ella.jpg">
+          </div>
         </div>
+      </div>
     </div>
-</div>
